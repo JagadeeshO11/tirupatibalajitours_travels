b7bb8c0d595a7b43b20e04419abf4452e0802eae
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-16 04:59:31 +0530

    Load mobile navigation fix styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,5 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './style.css';
 import './mobile-fixes.css';
+import './mobile-nav-fix.css';
 createRoot(document.getElementById('root')).render(<BrowserRouter><App/></BrowserRouter>);
