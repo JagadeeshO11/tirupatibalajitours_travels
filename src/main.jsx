1888a0ffee5f29089ff8dc8b260d012b68f5d437
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-15 12:42:10 +0530

    Apply desktop home card row centering

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,5 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './style.css';
+import './home-card-alignment.css';
 createRoot(document.getElementById('root')).render(<BrowserRouter><App/></BrowserRouter>);
