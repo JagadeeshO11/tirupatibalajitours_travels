0f3e8318ff0a6d07a7c77e2eb44b175be7e614c2
OsuruJagadeesh <157037205+JagadeeshO11@users.noreply.github.com>
2026-09-15 12:53:22 +0530

    Enable 3D home showcase carousel styling

--- src/main.jsx
+++ src/main.jsx
@@ -6,4 +6,5 @@ import './style.css';
 import './home-card-alignment.css';
 import './package-showcase.css';
 import './site-interaction.css';
+import './home-3d-carousel.css';
 createRoot(document.getElementById('root')).render(<BrowserRouter><App/></BrowserRouter>);
